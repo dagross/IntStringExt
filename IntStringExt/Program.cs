10bdae1c98ca7118399bba1cf38ffcd2faf9fefb
student <student@step.lan>
2017-09-21 19:49:14 +0300

    String Extension Method

--- IntStringExt/Program.cs
+++ IntStringExt/Program.cs
@@ -10,8 +10,24 @@ namespace IntStringExt
     {
         static void Main(string[] args)
         {
+            
+            string x = "Hello";
+            x.Y(23);
+            x.X();
+        }
+    }
+    static class Red
+    {
+        public static void X(this string value)
+        {
+            Console.WriteLine(value);
         }
 
 
-    }
+    public static void Y(this string value,int l)
+        {
+            Console.WriteLine(value + l);
+        }
+}
+
 }
