59f34dc1fb62eaffc1d1c9d1ecbc89b2a9d683b1
student <student@step.lan>
2017-09-21 19:42:47 +0300

    Int Extensions

--- IntStringExt/Program.cs
+++ IntStringExt/Program.cs
@@ -6,12 +6,33 @@ using System.Threading.Tasks;
 
 namespace IntStringExt
 {
+       
     class Program
     {
+
         static void Main(string[] args)
         {
+            int k = 15;
+            Console.WriteLine( k.IsOdd());
+            Console.WriteLine(k.AddNum(12));
+            string kl = "Hello World";
+        
         }
 
 
+    }
+
+    public static class IntExtensions
+    {
+        public static bool IsOdd(this int k)
+        {
+           
+            return k % 2 == 0;
+        }
+
+        public static double AddNum(this int l,double k)
+        {
+            return k + l;
+        }
     }
 }
